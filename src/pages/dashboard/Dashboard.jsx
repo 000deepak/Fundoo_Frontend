c0d6ea5c7040f5c8eb0ea2e79194f920e4c681e7
000deepak <kumbhardeepak000@gmail.com>
2022-01-28 17:25:41 +0530

    [deepak]Added:dashboard page

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,124 +1,121 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
-import RefreshOutlinedIcon from '@mui/icons-material/RefreshOutlined';
-import ViewStreamOutlinedIcon from '@mui/icons-material/ViewStreamOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+import RefreshOutlinedIcon from "@mui/icons-material/RefreshOutlined";
+import ViewStreamOutlinedIcon from "@mui/icons-material/ViewStreamOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
-import '../dashboard/Dashboard.scss'
+import "../dashboard/Dashboard.scss";
 import Notes from "../notes/Notes";
 
 const drawerWidth = 240;
 
-
-let iconLists=[
-    {
-    iconNames:<LightbulbOutlinedIcon/>,
-    iconTexts:"Notes"
-    },
-    {
-    iconNames:<NotificationsOutlinedIcon/>,
-    iconTexts:"Reminders"
-    },
-    {
-    iconNames:<ModeEditOutlineOutlinedIcon/>,
-    iconTexts:"Edit labels"
-    },
-    {
-    iconNames:<ArchiveOutlinedIcon/>,
-    iconTexts:"Archive"
-    },
-    {
-    iconNames:<DeleteOutlineOutlinedIcon/>,
-    iconTexts:"Spam"
-    },
-
-]
+let iconLists = [
+  {
+    iconNames: <LightbulbOutlinedIcon />,
+    iconTexts: "Notes",
+  },
+  {
+    iconNames: <NotificationsOutlinedIcon />,
+    iconTexts: "Reminders",
+  },
+  {
+    iconNames: <ModeEditOutlineOutlinedIcon />,
+    iconTexts: "Edit labels",
+  },
+  {
+    iconNames: <ArchiveOutlinedIcon />,
+    iconTexts: "Archive",
+  },
+  {
+    iconNames: <DeleteOutlineOutlinedIcon />,
+    iconTexts: "Spam",
+  },
+];
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(9)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     // width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
+    whiteSpace: "nowrap",
+    boxSizing: "border-box",
     ...(open && {
       ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
+      "& .MuiDrawer-paper": openedMixin(theme),
     }),
     ...(!open && {
       ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
+      "& .MuiDrawer-paper": closedMixin(theme),
     }),
-  }),
+  })
 );
-
 
 export default function MiniDrawer() {
   const theme = useTheme();
@@ -133,7 +130,7 @@ export default function MiniDrawer() {
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar>
@@ -142,69 +139,54 @@ export default function MiniDrawer() {
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            // sx={{
-            //   marginRight: '36px',
-            //   ...(open && { display: 'none' }),
-            // }}
           >
             <MenuIcon />
           </IconButton>
           <div className="mainHeader">
-          <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="logo" />
+            <img
+              src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
+              alt="logo"
+            />
 
-          <Typography variant="h5" noWrap component="div">
-           Fundoo
-          </Typography>
-          <div className="searchBar">
+            <Typography variant="h5" noWrap component="div">
+              Fundoo
+            </Typography>
+            <div className="searchBar">
               <SearchOutlinedIcon />
               <input className="search" type="text" placeholder="Search"></input>
             </div>
             <div className="headBar">
               <ul className="headerIcon">
-                <IconButton> <RefreshOutlinedIcon /> </IconButton>
-                <IconButton> <ViewStreamOutlinedIcon /> </IconButton>
-                <IconButton> <SettingsOutlinedIcon /></IconButton>
-                <IconButton> <AppsOutlinedIcon /></IconButton>
-                <IconButton> <AccountCircleOutlinedIcon /></IconButton>
+                <IconButton>
+                  {" "}
+                  <RefreshOutlinedIcon />{" "}
+                </IconButton>
+                <IconButton>
+                  {" "}
+                  <ViewStreamOutlinedIcon />{" "}
+                </IconButton>
+                <IconButton>
+                  {" "}
+                  <SettingsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                  {" "}
+                  <AppsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                  {" "}
+                  <AccountCircleOutlinedIcon />
+                </IconButton>
               </ul>
             </div>
-         </div>
+          </div>
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          {/* <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton> */}
-        </DrawerHeader>
-        {/* <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider /> */}
-        {/* <List>
-          {['Notes','Reminders', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-              {/* </ListItemIcon> */}
-              {/* <ListItemText primary={text} /> */}
-            {/* </ListItem> */}
-          {/* ))} */}
-        {/* </List> */}
         <List>
           {iconLists.map((text, index) => (
             <ListItem button key={text.iconNames}>
-              <ListItemIcon>
-                  {text.iconNames}
-              </ListItemIcon>
+              <ListItemIcon>{text.iconNames}</ListItemIcon>
               <ListItemText primary={text.iconTexts} />
             </ListItem>
           ))}
@@ -212,7 +194,7 @@ export default function MiniDrawer() {
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-  
+
         {/* <Notes/> */}
       </Box>
     </Box>
